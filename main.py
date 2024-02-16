3ff6e2a7e46fce2adc363c9671dfc658584ee84a
PeterSolution <141070789+PeterSolution@users.noreply.github.com>
2024-02-16 16:22:00 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,17 @@ file_path = filedialog.askopenfilename(
     title="select file",
     filetypes=[("Excel", "*.xlsx"), ("Word", "*.docx"), ("all files", "*.*")]
 )
+excelheight=0
+excelwidth=0
+truedatah=0
+truedatahw=0
 
+checkifemplty=0
+checkifempltywidth=0
+checkwidth=0
+checkheight=0
+forwidth=100
+forheight=100
 if file_path:
     print("file:", file_path)
     str = file_path.rfind('/')
@@ -25,14 +35,30 @@ if file_path:
         name=excel.sheetnames
         for data in name:
             data = excel[data]
+            for i in range(forwidth):
 
-            # Dalsza część kodu
-            for i in range(10):
-                for j in range(10):
+                for j in range(forheight):
                     value = data.cell(row=i + 1, column=j + 1).value
                     if value is not None:
                         print(f'value from cell ({i + 1},{j + 1}): {value}')
-
+                        if excelwidth < j:
+                            excelwidth = j
+                        if excelheight < i:
+                            excelheight = i
+                        checkifemplty = 0
+                        if (forheight - j) < 10:
+                            forheight = forheight + 1
+                    if value is None:
+                        checkifemplty = checkifemplty + 1
+                        if j < 11 & checkifemplty == 10:
+                            checkifempltywidth = checkifempltywidth + 1
+                        else:
+                            if j > 11 & checkifemplty == 10:
+                                break
+                    #     if j > excelwidth:
+                    #         excelwidth = j
+                    # if i > excelheight:
+                    #     excelheight = i
             #         print(value)
             #
             # print(sub)
@@ -40,3 +66,37 @@ if file_path:
 
     finally:
         excel.close()
+
+# checkifemplty=0
+# checkifempltywidth=0
+# checkwidth=0
+# checkheight=0
+# forwidth=100
+# forheight=100
+# while checkifempltywidth==10:
+#     excel = openpyxl.load_workbook(file_path)
+#     name = excel.sheetnames
+#     for data in name:
+#         data = excel[data]
+#         for i in range(forwidth):
+#
+#             for j in range(forheight):
+#                 value = data.cell(row=i + 1, column=j + 1).value
+#                 if value is not None:
+#                     print(f'value from cell ({i + 1},{j + 1}): {value}')
+#                     if excelwidth<j:
+#                         excelwidth=j
+#                     if excelheight<i:
+#                         excelheight=i
+#                     checkifemplty=0
+#                     if (forheight-j)<10:
+#                         forheight=forheight+1
+#                 if value is None:
+#                     checkifemplty=checkifemplty+1
+#                     if j <10&checkifemplty==10:
+#                         checkifempltywidth=checkifempltywidth+1
+#                     else:
+#                         if j >10&checkifemplty==10:
+#                             break
+print(excelheight)
+print(excelwidth)
